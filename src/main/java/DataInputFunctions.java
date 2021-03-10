961c861d57053605258c1680e4d34b93a86fb00b
VincentPreikstas <43098530+VincentPreikstas@users.noreply.github.com>
2021-03-10 18:09:30 -0500

    Small Update on Functions

--- src/main/java/DataInputFunctions.java
+++ src/main/java/DataInputFunctions.java
@@ -5,12 +5,13 @@ public class DataInputFunctions {
     }
 
 
-
+    //Nazar
     public String catInsertSQLGenerator (String catName, String catDescription, int catStockNum, boolean saleBool, String picURI){
         return "INSERT INTO database (catName, catDescription, catStockNum, saleBool, picURI) VALUES " +
                 "(" + catName + ", " + catDescription + ", " + catStockNum + ", " + saleBool + ", " + picURI + ");";
     }
     /*
+    //DONT DO
     public void catInstantiator (){
         // Calls catInsertSQLGenerator a lot to generate then execute sql queries
         // may require connection to db with a db object as an argument
@@ -18,12 +19,13 @@ public class DataInputFunctions {
     }
 
 
-
+    //Nazar
     public String locationInsertSQLGenerator (int locationID, int aisle, int rack, String shelf, String side){
         String SQLStatement;
         return SQLStatement;
     }
 
+    //DONT DO
     public void locationInstantiator(){
         // Calls Location InterSQL Generator a lot to generate and then execute sql queries
         // Will Likely require a connection object to run queries to the DB
@@ -31,23 +33,25 @@ public class DataInputFunctions {
     }
 
 
-
+    //Rudy
     public String travelNodeInsertGenerator (int nodeID, int northNodeID, int northNodeDistance, int etc){
         String SQLStatement;
         return SQLStatement;
     }
 
-
+    //Rudy
     public String shoppingListInsertGenerator(){
         String SQLStatement;
         return SQLStatement;
     }
 
+    //Matt
     public String userInsertGenerator(){
         String SQLStatement;
         return SQLStatement;
     }
 
+    //Matt
     public String addItemToGroceryListInsertGenerator(){
         String SQLStatement;
         return SQLStatement;
