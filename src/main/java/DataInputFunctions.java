fb98bf67d14d0774035e528056aae8bd722550d7
VincentPreikstas <43098530+VincentPreikstas@users.noreply.github.com>
2021-03-10 16:13:20 -0500

    Various Things Added
    Added SQL to txt file,
    example of reading excel in java,
    skeleton functions

---
+++ src/main/java/DataInputFunctions.java
@@ -0,0 +1,57 @@
+public class DataInputFunctions {
+
+    public DataInputFunctions (){
+
+    }
+
+
+    /*
+    public String catInsertSQLGenerator (String catName, String catDescription, int catStockNum, boolean saleBool, String picURI){
+        String SQLStatement;
+        return SQLStatement;
+    }
+    public void catInstantiator (){
+        // Calls catInsertSQLGenerator a lot to generate then execute sql queries
+        // may require connection to db with a db object as an argument
+        // may require file object to read and parse excel sheet if not done with another function and either passed in here or called here
+    }
+
+
+
+    public String locationInsertSQLGenerator (int locationID, int aisle, int rack, String shelf, String side){
+        String SQLStatement;
+        return SQLStatement;
+    }
+
+    public void locationInstantiator(){
+        // Calls Location InterSQL Generator a lot to generate and then execute sql queries
+        // Will Likely require a connection object to run queries to the DB
+        // May require a file object to read and parse excel sheet (or this could be broken up into other functions)
+    }
+
+
+
+    public String travelNodeInsertGenerator (int nodeID, int northNodeID, int northNodeDistance, int etc){
+        String SQLStatement;
+        return SQLStatement;
+    }
+
+
+    public String shoppingListInsertGenerator(){
+        String SQLStatement;
+        return SQLStatement;
+    }
+
+    public String userInsertGenerator(){
+        String SQLStatement;
+        return SQLStatement;
+    }
+
+    public String addItemToGroceryListInsertGenerator(){
+        String SQLStatement;
+        return SQLStatement;
+    }
+
+
+     */
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,56 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Hellow World!");
+
+
+    public static void main (String[] args)throws IOException{
+
+
+        //FOLLOWING EXAMPLE TAKEN FROM https://www.codejava.net/coding/how-to-read-excel-files-in-java-using-apache-poi
+        //Example shows how to iterate over every box in an excel sheet row by row left to right
+
+        String excelFilePath = "GroceryStoreData.xlsx";
+        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        Sheet firstSheet = workbook.getSheetAt(0);
+        Iterator<Row> iterator = firstSheet.iterator();
+
+        while (iterator.hasNext()){
+            Row nextRow = iterator.next();
+            Iterator<Cell> cellIterator = nextRow.cellIterator();
+            while (cellIterator.hasNext()){
+                Cell cell = cellIterator.next();
+                switch (cell.getCellType()){
+                    case STRING:
+                        System.out.print(cell.getStringCellValue());
+                        break;
+                    case BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue());
+                        break;
+                    case NUMERIC:
+                        System.out.print(cell.getNumericCellValue());
+                        break;
+
+                }
+                System.out.println(" - ");
+            }
+            System.out.println();
+        }
+        workbook.close();
+        inputStream.close();
+        //END EXAMPLE FROM https://www.codejava.net/coding/how-to-read-excel-files-in-java-using-apache-poi
+
+
     }
 }
